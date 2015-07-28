3b8f23ffccc1d7171e4f56568e06cf7c96458e3d
さくふわ <skfwMelonpan@gmail.com>
2015-07-28 12:08:57 +0900

    GPS

---
+++ GPS.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import serial
+import time
+import datetime
+
+ser=serial.Serial("/dev/ttyAMA0",9600)
+
+start = datetime.datetime.now()
+end = start+datetime.timedelta(seconds=60)
+
+print start
+
+while True:
+	line=ser.readline()
+	print "%s,%s"%(datetime.datetime.now()-start,line)
+	if end-datetime.datetime.now()<datetime.timedelta(seconds=0):
+		sys.exit(0)
+		
+sys.exit(0)
